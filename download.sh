3c6563320c5ff5cdb4ea7afaee05f8d0d7bcafea
Tim Peters <tim@catminion.net>
2024-12-23 20:33:31 +1030

    Use curl instead of w3m

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #
 # Download any missing input to input/YYYY/dayN.txt
-# Assumes w3m is already logged into adventofcode.com
+# Cookie grabbed from firefox, must already be logged in to adventofcode.com
 
 set -e
 
@@ -12,6 +12,11 @@ export TZ=US/Eastern
 this_year=$(date '+%Y')
 today=$(date '+%Y%m%d')
 
+cookies=$(mktemp /tmp/XXXXXXXXXX.sqlite)
+find "$HOME/.var/app/org.mozilla.firefox/.mozilla/firefox" -name cookies.sqlite -exec cp {} "$cookies" \;
+session=$(sqlite3 "$cookies" "select value from moz_cookies where name = 'session' and host = '.adventofcode.com'")
+rm -f "$tempfile"
+
 for year in $(seq 2015 "$this_year"); do
     for day in $(seq 25); do
         if [ "$(printf "%4d%02d%02d" "$year" 12 "$day")" -le "$today" ]; then
@@ -20,7 +25,7 @@ for year in $(seq 2015 "$this_year"); do
                 mkdir -p "input/$year"
                 url="https://adventofcode.com/$year/day/$day/input"
                 echo "Downloading $url -> $fn"
-                w3m "$url" > "$fn"
+                curl -sS -b session="$session" "$url" -o $fn
             fi
         fi
     done
